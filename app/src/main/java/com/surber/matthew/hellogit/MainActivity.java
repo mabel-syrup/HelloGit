9f458cbfc85463de1c4d17216b17cc6e96b3c275
mjSurber <matthewsurber96@gmail.com>
2016-09-06 14:33:11 -0500

    Fixing uncommitted file?

--- app/src/main/java/com/surber/matthew/hellogit/MainActivity.java
+++ app/src/main/java/com/surber/matthew/hellogit/MainActivity.java
@@ -2,6 +2,11 @@ package com.surber.matthew.hellogit;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +14,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button timeButton = (Button) findViewById(R.id.button);
+        timeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String currentTime = new Date().toString();
+                Toast.makeText(MainActivity.this, "The time is " + currentTime, Toast.LENGTH_LONG).show();
+            }
+        });
     }
     //Comment
 }
